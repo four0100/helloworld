7fb92514fe1a7d43f55246b784e305979658968a
four <suroot@gmail.com>
2011-08-25 15:33:42 -0700

    Initial add of dummy rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helloworld::Application.config.secret_token = '2b39bdf0bf09697be16247219eb02cf213d392bb7dfa278910b68c1d4e5fe882b9f2e9e58fe631b09fb8d6d02663ea86419f5ca2883f7ceb4953355519b3da8d'
